d0d9bfd71b91d685f1e8f87de94441cfb1145371
umbsa <sdmktkg@gmail.com>
2021-05-04 16:44:08 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one :staff
+
+  with_options presence: true do
+    validates :nickname
+    validates :user_birth_date
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+     validates :full_name
+    end
+    with_options format: {with: /\A[ァ-ヶー]+\z/} do
+      validates :full_name_kana
+     end
+  end
+
+end
