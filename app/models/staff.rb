c3405d92528b11b3f05a4aec3a273f5ddccb78aa
umbsa <sdmktkg@gmail.com>
2021-05-18 16:03:37 +0900

    バリデーションの追加修正

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -10,7 +10,10 @@ class Staff < ApplicationRecord
     validates :image
     validates :name
     validates :comment
-    validates :history
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/,
+      message: "は半角数字のみ使えます" }
+    validates :history, format: {with: /\A[0-9]+\z/, message: "は半角数字で入力してください"}
+    validates :hobby
   end
 
   def self.search(search)
